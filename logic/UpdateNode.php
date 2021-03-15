d521888f2986717cadb81e338919a5ed9f88c3a3
Jlbrito <joseluisbrito@gmail.com>
2021-03-14 23:16:40 -0300

    Added UpdateNode to logic layer

---
+++ logic/UpdateNode.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Logic;
+
+use Entities\Node;
+use Controllers\TreeController;
+
+class UpdateNode
+{
+    public function __construct()
+    {
+        
+    }
+    
+    public function updateNode(int $node_id, string $new_name)
+    {
+        try {
+            $treeController = new TreeController();
+            $node = $treeController->findNodeById($node_id);
+            $node->setName($new_name);
+            $treeController->setParentNode($node);
+        } catch (Exception $ex) {
+            throw $ex;
+        }
+    }
+}
